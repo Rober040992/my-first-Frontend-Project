ef9ef9bdca1b5624e485f576ecd987fa206b01cc
RoberGF <rgfrasta@gmail.com>
2024-11-18 18:22:51 +0100

    adding a loader/spinner between the add data loading

--- Adds-MVC/controller.js
+++ Adds-MVC/controller.js
@@ -2,14 +2,21 @@ import { getAdds } from "./model.js";
 import { drawAdds } from "./drawAddcontroller.js";
 
 async function printAdds() {
-    const addsContainer = document.querySelector('.adds-container');
-    addsContainer.innerHTML = "";
-    try {
-      const adds = await getAdds();
-      drawAdds(adds, addsContainer)
-    } catch (error) {
-      alert(error.message)
-    }
+  const addsContainer = document.querySelector('.adds-container');
+  const loader = document.querySelector(".loader")
+  addsContainer.innerHTML = "";
+
+  loader.classList.toggle("hidden")
+  try {
+    const adds = await getAdds();
+    drawAdds(adds, addsContainer)
+
+  } catch (error) {
+    alert(error.message)
+
+  } finally {
+    loader.classList.toggle("hidden")
   }
-  
-  document.addEventListener("DOMContentLoaded", printAdds);
+}
+
+document.addEventListener("DOMContentLoaded", printAdds);
